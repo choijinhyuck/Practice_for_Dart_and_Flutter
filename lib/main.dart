453d93c3de59fbe8b9202fe95119483ee2be8571
choijinhyuck <choijinhyuck@naver.com>
2023-04-21 23:50:09 +0900

    버튼 위젯 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,82 @@
-Future<void> Books (String first, String second) async {
-  print("$first 처음 문장");
-  await Future.delayed(Duration(seconds:3),(){
-    print("$first 이건 await 문장");
-  });
-  print("$second 두번째 문장");
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp4());
 }
 
-void main () async {
-  await Books("Dart","Flutter");
-  await Books("Python","FastAPI");
+class MyApp4 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+            child: Text("엘리베이티드 버튼"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            child: Text("아웃라인드 버튼"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            child: Text("텍스트 버튼"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            '텍스트 위젯입니다!!!',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.blue,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
