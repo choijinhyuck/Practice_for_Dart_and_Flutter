015ccc01780927e7db12934d52d7da139e2cbad8
choijinhyuck <choijinhyuck@naver.com>
2023-04-22 00:13:13 +0900

    GestureDetector on Container widget 연습

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp4());
-}
-
-class MyApp4 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.red,
-            ),
-            child: Text("엘리베이티드 버튼"),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyApp3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: OutlinedButton(
-            onPressed: () {},
-            style: OutlinedButton.styleFrom(
-              foregroundColor: Colors.red,
-            ),
-            child: Text("아웃라인드 버튼"),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyApp2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              foregroundColor: Colors.red,
-            ),
-            child: Text("텍스트 버튼"),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -67,12 +10,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(
-            '텍스트 위젯입니다!!!',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-              color: Colors.blue,
+          child: GestureDetector(
+            onTap: () {
+              print('on tap');
+            },
+            onDoubleTap: () {
+              print('on double tap');
+            },
+            onLongPress: () {
+              print('on long press');
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              width: 100.0,
+              height: 100.0,
             ),
           ),
         ),
