7cc822bc74097d4bfed20b21d78d1ac02a051cb3
choijinhyuck <choijinhyuck@naver.com>
2023-04-23 08:32:19 +0900

    PageView를 활용한 이미지 Swipe 가능 화면 구현, 상태바 색상 변경

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return Scaffold(
-      body: Text('홈 스크린'),
+      body: PageView(
+        children: [1, 2, 3]
+            .map((e) => Image.asset(
+                  'assets/images/image_$e.jpg',
+                  fit: BoxFit.cover,
+                ))
+            .toList(),
+      ),
     );
   }
 }
